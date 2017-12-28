a4976306400271623eb5a30a1e8bffa14eda7253
erheng <957067346@qq.com>
2017-12-28 16:48:29 +0800

    Initial Commit

---
+++ 规格/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  规格
+//
+//  Created by aa on 2017/12/28.
+//  Copyright © 2017年 weiyankeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 规格/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  规格
+//
+//  Created by aa on 2017/12/28.
+//  Copyright © 2017年 weiyankeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
